40a15f4b51ac3030d7e907ae79ee706659efc645
Giovanni Rivera <rivera.giovanni271@gmail.com>
2025-10-22 19:14:03 -0700

    ♻ Refactor: Cleanup DualTrits.h and main.cpp output

--- DualTrits.h
+++ DualTrits.h
@@ -4,18 +4,27 @@
 
 #ifndef PROJECT_FLOAT_DUALTRITS_H
 #define PROJECT_FLOAT_DUALTRITS_H
+
 #include <cmath>
 #include <cstdint>
 #include <iostream>
 #include <limits>
 #include <ostream>
-#include "mpreal.h"
-using mpfr::mpreal;
+#include <sstream>
+#include <mpreal.h>
+
+typedef int8_t wide_t;
+
 class DualTrits {
 public:
-    using wide_t =  int8_t;
     static constexpr wide_t BASE = 3;
     constexpr explicit DualTrits(wide_t m = 0, int e = 0) noexcept : exponent(e), mantissa(m) {
+    }
+
+    std::string toString() {
+        std::ostringstream oss;
+        oss << "exponent = " << this->exponent << ", mantissa = " << this->mantissa;
+        return oss.str();
     }
 
     float toFloat() {
@@ -25,6 +34,15 @@ public:
     double toDouble() {
         return to<double>();
     }
+
+    std::string toFloatString() {
+        return toAsString<float>();
+    }
+
+    std::string toDoubleString() {
+        return toAsString<double>();
+    }
+
     DualTrits operator+(const DualTrits& other) const {
         //exact compute
         return DualTrits{};
@@ -41,26 +59,30 @@ public:
         //exact compute
         return DualTrits{};
     }
-    mpreal toMPreal() const {
+
+    mpfr::mpreal toMPreal() const {
         if (this->exponent == 0) {
             if (this->mantissa == 0) {
-                return mpreal{0};
+                return mpfr::mpreal{0};
             }
             if (this->mantissa == 1) {
-                mpreal inf;
+                mpfr::mpreal inf;
                 inf.setInf();
                 return inf;
             }
-            mpreal neginf;
+            mpfr::mpreal neginf;
             neginf.setInf(-1);
-            return mpreal{neginf};
+            return mpfr::mpreal{neginf};
         }
-        mpreal base{BASE};
-        mpreal mantissa{reinterpt_digit(this->mantissa)};
-        mpreal exponent{reinterpt_digit(this->exponent)};
+        mpfr::mpreal base{BASE};
+        mpfr::mpreal mantissa{reinterpt_digit(this->mantissa)};
+        mpfr::mpreal exponent{reinterpt_digit(this->exponent)};
         return mantissa * mpfr::pow(base,exponent);
     }
 
+    std::string toMPrealString() {
+        return toMPreal().toString();
+    }
 
 private:
     template<typename T>
@@ -77,8 +99,33 @@ private:
         auto reinterped_mantissa = reinterpt_digit(mantissa);
         T convertedMantissa = static_cast<T>(reinterped_mantissa);
         T convertedExponent = pow_base<T, BASE>(exponent);
-        std::cout << convertedExponent << " * " << convertedMantissa << " = ";
+        // std::cout << convertedExponent << " * " << convertedMantissa << " = ";
         return convertedMantissa * convertedExponent;
+    }
+
+    template<typename T>
+    [[nodiscard]] std::string toAsString() const noexcept {
+        std::ostringstream oss;
+
+        if (this->exponent == 0) {
+            if (this->mantissa == 0) {
+                oss << T(0);
+                return oss.str();
+            }
+            if (this->mantissa == 1) {
+                oss << std::numeric_limits<T>::max();
+                return oss.str();
+            }
+            oss << std::numeric_limits<T>::lowest();
+            return oss.str();
+        }
+        auto reinterped_mantissa = reinterpt_digit(mantissa);
+        T convertedMantissa = static_cast<T>(reinterped_mantissa);
+        T convertedExponent = pow_base<T, BASE>(exponent);
+
+        T result = convertedMantissa * convertedExponent;
+        oss << convertedExponent << " * " << convertedMantissa << " = " << result;
+        return oss.str();
     }
 
     [[nodiscard]] static wide_t constexpr reinterpt_digit(wide_t digit) noexcept {
@@ -105,9 +152,8 @@ private:
     }
 
 
-    __uint128_t exponent;
-    __uint128_t mantissa;
+    unsigned int exponent : 2;
+    unsigned int mantissa : 2;
 };
-
 
 #endif //PROJECT_FLOAT_DUALTRITS_H
