6e4905731edfd33f7cbf0e95d018b76324f41d75
Giovanni Rivera <rivera.giovanni271@gmail.com>
2025-11-20 16:43:04 -0800

    ♻ Refactor: Add -DUSE_MPFR definition to cmake and USE_MPFR macro

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,9 @@ int main() {
             std::cout << "\tAs Packed Bits = "<< digit.asPackedBits().to_string() << std::endl;
             std::cout << "\tRaw Value = "<< digit.asRawPackedBits() << std::endl;
             std::cout << "\tDouble = "<< digit.toDoubleString() << std::endl;
+#ifdef USER_MPFR
             std::cout << "\tMPreal = "<< digit.toMPrealString() << std::endl;
+#endif
             std::cout << std::endl;
         }
     }
